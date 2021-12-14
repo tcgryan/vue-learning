fe4125bc5227b7a789d5469e6e4e0b8e426b5087
Ryan Hickey <ryan.hickey@tcgp.onmicrosoft.com>
2021-12-14 14:41:56 -0500

    lecture33

---
+++ lecture33/basics-10-styling-starting-setup/app.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
+    };
+  },
+  methods: {
+    boxSelected(box) {
+      if (box === "A") {
+        this.boxASelected = !this.boxASelected;
+      } else if (box === "B") {
+        this.boxBSelected = !this.boxBSelected;
+      } else {
+        this.boxCSelected = !this.boxCSelected;
+      }
+    },
+  },
+  computed: {
+    boxAClasses() {
+      return { active: this.boxASelected };
+    },
+    boxBClasses() {
+      return { active: this.boxBSelected };
+    },
+    boxCClasses() {
+      return { active: this.boxCSelected };
+    },
+  },
+});
+
+app.mount("#styling");
