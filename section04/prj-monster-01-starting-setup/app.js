9b22aedb1b81ca1392291b524b12218115a4619f
Ryan Hickey <rhickey94@gmail.com>
2021-12-15 09:06:11 -0500

    added end game support

--- section04/prj-monster-01-starting-setup/app.js
+++ section04/prj-monster-01-starting-setup/app.js
@@ -8,6 +8,7 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 1,
+      winner: null,
     }
   },
   methods: {
@@ -56,12 +57,27 @@ const app = Vue.createApp({
         `Heal! player: ${this.playerHealth}, monster: ${this.monsterHealth}`,
       )
     },
+    startNewGame() {
+      this.playerHealth = 100
+      this.monsterHealth = 100
+      this.currentRound = 1
+      this.winner = null
+    },
+    surrender() {
+      this.winner = 'monster'
+    },
   },
   computed: {
     currentPlayerHealth() {
+      if (this.playerHealth < 0) {
+        return { width: `0%` }
+      }
       return { width: `${this.playerHealth}%` }
     },
     currentMonsterHealth() {
+      if (this.monsterHealth < 0) {
+        return { width: `0%` }
+      }
       return { width: `${this.monsterHealth}%` }
     },
     specialAttackCooldown() {
@@ -69,6 +85,22 @@ const app = Vue.createApp({
     },
     healCooldown() {
       return this.currentRound % 4 !== 0
+    },
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = 'draw'
+      } else if (value <= 0) {
+        this.winner = 'monster'
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        this.winner = 'draw'
+      } else if (value <= 0) {
+        this.winner = 'player'
+      }
     },
   },
 })
