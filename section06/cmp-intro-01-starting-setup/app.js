089b0bd0c63940b36c078e6e752e5ba254cb3692
Ryan Hickey <ryan.hickey@tcgp.onmicrosoft.com>
2021-12-16 15:04:46 -0500

    new section

---
+++ section06/cmp-intro-01-starting-setup/app.js
@@ -0,0 +1,56 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "09876 543 221",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+});
+
+app.component("friend-contact", {
+  template: `
+    <li>
+      <h2> {{ friend.name }}</h2>
+      <button @click="toggleDetails">{{ detailVisibility }} Details</button>
+      <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+      </ul>
+    </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: true,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "01234 5678 991",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  computed: {
+    detailVisibility() {
+      return this.detailsAreVisible ? 'Hide' : 'Show'
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
+
+app.mount("#app");
